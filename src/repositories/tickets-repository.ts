bfcf2b7d4f39c82d65f7f1f118e9ad780aafd029
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-09-22 20:48:47 -0300

    fix:remove unnecessary awaits

--- src/repositories/tickets-repository.ts
+++ src/repositories/tickets-repository.ts
@@ -2,27 +2,27 @@ import { Ticket, TicketType } from '@prisma/client';
 import { prisma } from '@/config';
 
 async function findTicket(userId: number): Promise<Ticket> {
-  return await prisma.ticket.findFirst({ where: { AND: { Enrollment: { userId } } }, include: { TicketType: true } });
+  return prisma.ticket.findFirst({ where: { AND: { Enrollment: { userId } } }, include: { TicketType: true } });
 }
 
 async function findTicketById(id: number) {
-  return await prisma.ticket.findUnique({ where: { id }, include: { Enrollment: true, TicketType: true } });
+  return prisma.ticket.findUnique({ where: { id }, include: { Enrollment: true, TicketType: true } });
 }
 
 async function findTicketTypes(): Promise<TicketType[]> {
-  return await prisma.ticketType.findMany();
+  return prisma.ticketType.findMany();
 }
 
 async function findTicketTypeById(id: number): Promise<TicketType> {
-  return await prisma.ticketType.findUnique({ where: { id } });
+  return prisma.ticketType.findUnique({ where: { id } });
 }
 
 async function create(data: CreateTicket): Promise<TicketAndType> {
-  return await prisma.ticket.create({ data: { ...data, status: 'RESERVED' }, include: { TicketType: true } });
+  return prisma.ticket.create({ data: { ...data, status: 'RESERVED' }, include: { TicketType: true } });
 }
 
 async function updateStatus(id: number) {
-  return await prisma.ticket.update({ where: { id }, data: { status: 'PAID' } });
+  return prisma.ticket.update({ where: { id }, data: { status: 'PAID' } });
 }
 
 export type CreateTicket = Omit<Ticket, 'id' | 'createdAt' | 'updatedAt' | 'status'>;
