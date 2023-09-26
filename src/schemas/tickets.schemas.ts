163a37d22f5ae18639a664d832ee9b79afe95067
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-09-26 19:06:23 -0300

    fix:more merge errors

--- src/schemas/tickets.schemas.ts
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-import { CreateTicketBody } from '@/repositories';
-
-export const ticketSchema = Joi.object<CreateTicketBody>({
-  ticketTypeId: Joi.number().integer().required(),
-});
